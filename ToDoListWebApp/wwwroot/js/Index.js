f824fa1ab06a4511360d1bcd3db5bedeb682a040
Dmitriy Krekshin <dima.krejshin@gmail.com>
2022-04-18 15:30:21 +0300

    реализовать отображение и закрытие окна редактирования задачи

--- ToDoListWebApp/wwwroot/js/Index.js
+++ ToDoListWebApp/wwwroot/js/Index.js
@@ -8,8 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskDoneButtons = document.querySelectorAll('button[id*="TaskDone"]');
     taskDoneButtons.forEach(n => n.addEventListener("click", taskDone));
     
-    // const editBtn = document.querySelectorAll('button[id*="EditTask"]');
-    // editBtn.forEach(btn => btn.addEventListener("click", showTaskBtn));
+    const editBtn = document.querySelectorAll('button[id*="EditTask"]');
+    editBtn.forEach(btn => btn.addEventListener("click", showTaskEditForm));
+    
+    const closeEditBtn = document.querySelectorAll('button[id*="Cancel"]');
+    closeEditBtn.forEach(btn => btn.addEventListener("click", hideTaskEditForm));
+    
 });
 
 function taskDone(e) {
@@ -23,4 +27,22 @@ function showTaskBtnGroup(e) {
 
 function hideTaskBtnGroup(e) {
     e.currentTarget.querySelector('div[id*="TaskBtnGroup"]').style.visibility = "hidden";
+}
+
+function showTaskEditForm(e) {
+    const taskId = e.currentTarget.attributes.taskId.value;
+    const taskEditPanel = document.querySelector(`div[id="{${taskId}}_TaskEditPanel"]`);
+    taskEditPanel.style.display = "block";
+    
+    const taskInfo = document.querySelector(`div[id="{${taskId}}_TaskInfo"]`);
+    taskInfo.style.display = "none";
+}
+
+function hideTaskEditForm(e) {
+    const taskId = e.currentTarget.attributes.taskId.value;
+    const taskEditPanel = document.querySelector(`div[id="{${taskId}}_TaskEditPanel"]`);
+    taskEditPanel.style.display = "none";
+    
+    const taskInfo = document.querySelector(`div[id="{${taskId}}_TaskInfo"]`);
+    taskInfo.style.display = "flex";
 }
